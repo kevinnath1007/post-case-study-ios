925ba98b4e352e95ccea86792621ed7736e9efc2
Kevin Nathaniel <kevin.nathaniel@sicepat.com>
2022-08-08 20:00:57 +0800

    Initial Commit

---
+++ post-study-case-ios/post_study_case_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  post_study_case_iosApp.swift
+//  post-study-case-ios
+//
+//  Created by Kevin Nathaniel on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct post_study_case_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
